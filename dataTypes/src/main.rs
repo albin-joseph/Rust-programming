eb10629994779d236fd4babe0c9d4677bd82e840
albin-joseph <albinjoseph007@gmail.com>
2025-06-19 17:42:06 +0530

    update rust types

--- dataTypes/src/main.rs
+++ dataTypes/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
+    //PRIMITIVE DATA TYPES
+
     //Int
     let x: i32 = -42;
     let y: u32 = 10;
@@ -16,4 +18,38 @@ fn main() {
     //char
     let letter: char = 'a';
     println!("first letter of the alphabets: {}", letter);
+
+    //COMPUND DATA TYPES
+    //arrays, tuples, slices and strings
+
+    //Arrays
+    //Array should contain homogenious items
+    //Array type should contain 2 values type and number of items
+    //[<type>, <no of items>]
+    let  numbers: [i32; 5] = [1,2,3,4,5];
+    println!("Number Array: {:?}", numbers);
+
+    let fruits: [&str; 3] = ["Apple", "banana", "orange"] ;
+    println!("Fruits Array : {:?}", fruits);
+    println!("Fruits Array at 0 : {}", fruits[0]);
+
+    //Tuples
+    //Tuple should contain hetrogeneous types
+    let human: (String, i32, bool) = ("Alice".to_string(), 30, false);
+    println!("Human Tuple: {:?}", human);
+
+    let my_mix_tuple: (&'static str, i32, bool, [i32; 5]) = ("Albin", 23, true, [1,2,3,4,5]);
+    println!("My mixed tuple: {:?}", my_mix_tuple);
+
+    //Slices: Dynamically sized elements contigenous allocation of memmory
+    let number_slices: &[i32] = &[1,2,3,4,5];
+    println!("Number Slices: {:?}", number_slices);
+
+    let animal_slices: &[&str] = &["elephent", "horse", "cow", "dog", "cat"];
+    println!("Animal Slices: {:?}", animal_slices);
+
+     let books_slices: &[&String] = &[&"elephent".to_string(), &"horse".to_string(), &"cow".to_string()];
+    println!("Books Slices: {:?}", books_slices);
+
+
 }
