65fbd710b48e8ec569cf8d3306361b48b08fefc6
albin-joseph <albinjoseph007@gmail.com>
2025-06-20 09:47:04 +0530

    update functions method

--- _03_functions/src/main.rs
+++ _03_functions/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let int_value = 2;
     let result = get_square_of_int(int_value);
     println!("Square of 2 is: {}", result);
+    human_id("Albin", 35, 165.0);
 }
 
 fn print_hello_world() {
@@ -16,4 +17,8 @@ fn print_hello_world() {
 
 fn get_square_of_int (value: i32) -> i32 {
     value * value
+}
+
+fn human_id(name: &str, age: i32, hieght: f64) {
+    println!("My name is {}, I have {} years old and my height is {}", name, age, hieght);
 }
