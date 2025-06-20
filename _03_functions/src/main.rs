d2479e35cf5d4d8c8e6b373362178663af305bd1
albin-joseph <albinjoseph007@gmail.com>
2025-06-20 09:40:01 +0530

    update function program

--- _03_functions/src/main.rs
+++ _03_functions/src/main.rs
@@ -4,9 +4,16 @@
 //parameters we can pass to the functions by variable reference 
 
 fn main() {
-    print_hello_world()
+    print_hello_world();
+    let int_value = 2;
+    let result = get_square_of_int(int_value);
+    println!("Square of 2 is: {}", result);
 }
 
 fn print_hello_world() {
     println!("Hello, world! from print_hello_world()");
+}
+
+fn get_square_of_int (value: i32) -> i32 {
+    value * value
 }
