b5e01cffecbf8ea8e769b111dc392126f1b578db
albin-joseph <albinjoseph007@gmail.com>
2025-06-20 15:15:37 +0530

    update functions code

--- _03_functions/src/main.rs
+++ _03_functions/src/main.rs
@@ -5,10 +5,17 @@
 
 fn main() {
     print_hello_world();
-    let int_value = 2;
-    let result = get_square_of_int(int_value);
+    let int_value: i32 = 2;
+    let result: i32 = get_square_of_int(int_value);
     println!("Square of 2 is: {}", result);
     human_id("Albin", 35, 165.0);
+
+    let weight: f64 = 60.5;
+    let height: f64 = 1.65;
+
+    let bmi: f64 = calculate_bmi(weight, height);
+    println!("Your BMI is {:.2}", bmi)
+
 }
 
 fn print_hello_world() {
@@ -26,3 +33,8 @@ fn human_id(name: &str, age: i32, hieght: f64) {
 //Expression & Statement
 //Expression: anything that returns a value
 //Statement: anything that does not returns a value
+//Almost all statements in Rust end with ;
+
+fn calculate_bmi(weight_kg: f64, height_m: f64) -> f64 {
+    weight_kg/(height_m*height_m)
+}
