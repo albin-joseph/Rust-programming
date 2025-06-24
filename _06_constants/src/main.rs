e93a10ab7cb9c4a83494767a50e341ddeab1b1fe
albin-joseph <albinjoseph007@gmail.com>
2025-06-24 21:42:46 +0530

    add constants program

---
+++ _06_constants/src/main.rs
@@ -0,0 +1,15 @@
+//Constants
+//const declare with a keyword const
+//const should be declare with type annotation
+fn main() {
+    println!("Constantants");
+    const Y: i32 = 10;
+
+    println!("The value of const Y is {}", Y);
+
+    println!("The value of PI is {}", PI);
+
+}
+
+//You can declare const here
+const PI: f64  = 3.14;
