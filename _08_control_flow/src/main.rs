f149c527c2736c05c6c65bbfb017062a63fd4cf5
albin-joseph <albinjoseph007@gmail.com>
2025-06-24 22:12:32 +0530

    add controlflow code

---
+++ _08_control_flow/src/main.rs
@@ -0,0 +1,13 @@
+//Control flow in RUust
+//1-consditions [Id...Else]
+//2-Repeating actions [Loops]
+fn main() {
+    println!("Control Flow");
+
+    let age : u16 = 18;
+    if age >= 18 {
+        println!("You can drive a car");
+    } else {
+         println!("You can't drive a car");
+    }
+}
