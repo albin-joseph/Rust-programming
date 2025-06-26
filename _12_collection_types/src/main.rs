422be5262bc4e5b2707a6babeb762d814614d47d
albin-joseph <albinjoseph007@gmail.com>
2025-06-26 15:13:13 +0530

    update collection program

--- _12_collection_types/src/main.rs
+++ _12_collection_types/src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections, string};
+
 fn main() {
     println!("Collection Types");
 
@@ -23,4 +25,39 @@ fn main() {
          Some(third) => println!("The third element for GET method is {third}"),
          None => println!("There is no third lement")
      }
+
+
+     //UTF8
+     let s = "hello".to_string();
+     let s: String = String::from("hello");
+     let mut s: String = String:: from("foo");
+     s.push_str("bar");
+     s.push('!');
+     
+     println!("the value of s is equal to {}", s);
+
+     let s1  = "Hello".to_string();
+     let s2 = " World".to_string();
+
+     let s3 = s1 + &s2;
+
+     println!("s2: {}", s3);
+
+     //HashMap
+
+     use std::collections::HashMap;
+     let mut scores: HashMap<String, i32> = HashMap::new();
+     scores.insert(String::from("Black"), 10);
+     scores.insert(String::from("White"), 11);
+
+     let team_name = String::from("Black");
+     let score = scores.get(&team_name).copied().unwrap_or(0);
+
+     for(key, value) in &scores {
+        println!("{key}: {value}")
+     }
+
+
+
+
 }
