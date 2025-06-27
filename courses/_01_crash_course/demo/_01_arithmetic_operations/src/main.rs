0936dcb2181c4fb0ef8647e174393eacc21a4e3b
albin-joseph <albinjoseph007@gmail.com>
2025-06-27 11:14:34 +0530

    add aritmetic operations

---
+++ courses/_01_crash_course/demo/_01_arithmetic_operations/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let sum = 2 + 2;
+    let value = 10 - 5;
+    let div_result = 10/2;
+    let mult = 5 * 2;
+
+    println!("Sum: {}, Substarction: {}, divison: {}, multiplication: {}", sum, value, div_result, mult);
+
+    println!("Sum: {}, Substarction: {}, divison: {}, multiplication: {}", addition(1,2), substraction(4,3), division(3,2), multiplication(5,2));
+}
+
+
+
+fn addition(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn substraction(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+fn multiplication(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+fn division(a: i32, b: i32) -> i32 {
+    a / b
+}
+
+
+
+
