28cf8f633355beb3608de14c9fe904cac26f4156
albin-joseph <albinjoseph007@gmail.com>
2025-07-08 06:13:52 +0530

    update match program

--- courses/_01_crash_course/demo/_03_match_expression/src/main.rs
+++ courses/_01_crash_course/demo/_03_match_expression/src/main.rs
@@ -6,4 +6,13 @@ fn main() {
         true => println!("It's true"),
         false => println!("it's false"),
     }
+
+    let some_int = 4;
+
+    match some_int {
+        1 => println!("Its 1"),
+        2 => println!("Its 2"),
+        3 => println!("Its 3"),
+        _ => println!("Its something else")
+    }
 }
