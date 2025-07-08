31f1d989b0c9f1d1cdfebab2e25b64102c0a5439
albin-joseph <albinjoseph007@gmail.com>
2025-07-08 13:23:13 +0530

    add expression demo program

---
+++ courses/_01_crash_course/demo/_06_expressions/src/main.rs
@@ -0,0 +1,18 @@
+enum Access{
+    Admin,
+    Manager,
+    User,
+    Guest
+}
+fn main() {
+    println!("<--------- Expressions --------->");
+    let access_level = Access::Guest;
+    let can_access_file = match access_level {
+        Access::Admin => true,
+        Access::Manager => true,
+        Access::User => false,
+        Access::Guest => false,
+    };
+
+    println!("Can acces files: {}", can_access_file);
+}
